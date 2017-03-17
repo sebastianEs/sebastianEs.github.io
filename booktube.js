e07f3b58036a39035c5193ea04c7eb7f1daa1884
sebastian <estradis_91@hotmail.com>
2017-03-17 11:11:56 +0100

    some more structure

--- booktube.js
+++ booktube.js
@@ -9,6 +9,8 @@ let bT ='', bA ='';
 let responseInput = '';
 let bookinputArray = [];
 let varning = document.getElementById('varning');
+
+
 /*       fetchfunktion    */
 let getFetchInsert = function () {
         fetch('https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key='+apiKey+'&title='+bookTitle.value+'&author='+bookAuthor.value)
@@ -22,11 +24,11 @@ let getFetchInsert = function () {
             
             // Check for error
             if (json.status == "error"){
-                console.log("En gång till");
+                
                 addBook();
             } else {
             addMoreBooks();
-                 console.log("SLUT");
+                 
             }
             })
 			.catch(function(error) {
@@ -44,10 +46,10 @@ function getRequestKey() {
         return response.json();
     }).then(function (json) {
         data.innerHTML = `${json.key}` + ' :';
-        console.log(json);
+    
         data.style.visibility = 'visible';
         apiKey = json.key;
-        console.log(apiKey);
+        
     })
 	.catch(function(error) {
 		console.log('Something went wrong! ' + error );
@@ -62,34 +64,29 @@ function getRequestKey() {
 			
 function addBook() {
     if (bookTitle.value == '' && bookAuthor.value == '') {
-        console.log('skriv något!');
-		varning.innerHTML ='Write something'
+     
+		varning.innerHTML ='Did you forget to search?'
     }
     else if (bookTitle.value == '') {
-        console.log('skriv en titel :');
-		varning.innerHTML ='Write any book title'
+  
+
+		varning.innerHTML ='Write any book title.'
     }
     else if (bookAuthor.value == '') {
-        console.log('skriv en författare :');
-		varning.innerHTML ='Write any Author.'
+      
+		varning.innerHTML ='Write an Author.'
     }
     else {
 		
         console.log('https://www.forverkliga.se/JavaScript/api/crud.php?op=insert&key='+apiKey+'&title='+bookTitle.value+'&author='+bookAuthor.value);
-       
+        
 	  if( apiKey === "" ) {
-		  varning.innerHTML ='Assign key!' 
+		  varning.innerHTML ='Missing Key, to make a search request api key.' 
 	  } else {
 		  getFetchInsert();
+		   varning.innerHTML ='';
 	  }
-        //Var ska errorChecker sättas in?
-        //Apichecker för inloggning
-        //Hämta böcker
-        
-            
-           // addBookInput.innerHTML = 'Titel : ' + bookTitle.value + ' :' + ' Author :' + bookAuthor.value;
-        console.log("Current saved books:: https://www.forverkliga.se/JavaScript/api/crud.php?op=select&key="+apiKey);
-        
+      
     }
 }
 
@@ -101,58 +98,36 @@ let addMoreBooks = function() {
           return response.json();
           })
     .then(function(json){
-        console.log(json);
+        
         let booksFromApiStringed = json;
         console.log(booksFromApiStringed);
-        console.log(json.status);
+      
         
         
     
-        //Check for error
+
         if (json.status == "error"){
-                console.log("Error from addMoreBooks");
+               
                 addMoreBooks();
             } else {
-                console.log("No error from addMoreBooks"+booksFromApiStringed);
+          
                    addBookInput.innerHTML='';
 				   for ( let i = 0; i < booksFromApiStringed.data.length; i++){
-                       console.log(booksFromApiStringed.data[i].title);
+                     
                        bT = booksFromApiStringed.data[i].title;
                        bA = booksFromApiStringed.data[i].author;
                     
-                       console.log("inne i forloop");
+                   
 					    let newInput = document.createElement('li');
                         newInput.innerHTML = "Titel: " +bT+ "  Author: "+ bA;
                         addBookInput.appendChild(newInput);
                 };
-                //Create element to NewInput
+         
                
             }
        });
 };
 
-
-
-/*function inputChecker() {
-	if( apiKey === '' ) {
-		
-		console.log('STOP!');
-		varning.innerHTML ='You need an api key to search for books!';
-		pickBtn.disabled = true;
-	} else {
-		pickBtn.disabled = false;
-		console.log('GO!');
-		getFetchInsert();
-	}
-}*/
-function changeBook() {
-    //  en function som ändrar input och återställer nuvarande bok.
-    //  när man klickar på knappen ska sidans innehåll ändras.
-}
-function removeAll() {
-    //  en function som tar bort allt och visar sidan som vid start.
-    //  när man klickar på knappen ska sidan återställas till startläge
-}
 
 /*''''''''''''''' KLICK EVENTS ''''''''''''''*/
 window.onload = function () {
@@ -170,10 +145,6 @@ window.onload = function () {
 	
 	
 };
-/*
-function deleteContent() {
-// en function som radderar den aktuella sökningen.
-}
-*/
+
 
 
