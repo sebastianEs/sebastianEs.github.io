7b78a0ba9fae60cdb4fdb0ba02005dcbebc54ab7
sebastian <estradis_91@hotmail.com>
2017-03-28 15:58:58 +0200

    test auth

--- Ny mapp/authcheck.js
+++ Ny mapp/authcheck.js
@@ -1,18 +1,41 @@
-
- var config = {
-    apiKey: "AIzaSyAFXG0D7RwP7VSISZ2PANf4ISN9Y1TmBDU",
-    authDomain: "authcheck-d66dd.firebaseapp.com",
-    databaseURL: "https://authcheck-d66dd.firebaseio.com",
-    storageBucket: "authcheck-d66dd.appspot.com",
-    messagingSenderId: "1053363999524"
-  };
-  firebase.initializeApp(config);
-
+/*!!!!!!!!!login!!!!!!!!!!!!!!!!!!!!!!*/
+ function logInUser() {
   let provider = new firebase.auth.GithubAuthProvider();
 
   firebase.auth().signInWithPopup(provider)
   .then(function(result) {
 	
 	let user = result.user;
-
+    console.log(result.user);
 	});
+	}
+	/*!!!!!!!!!!!!!logout!!!!!!*/
+	function logOutUser() {
+	firebase.auth().signOut()
+    .then(function(result) {
+	// Utloggning lyckades
+  })
+   .catch(function(error) {
+	// Utloggning misslyckades
+  });
+  }
+  
+  let authBtn = document.getElementById('auth-btn');
+  let showImg = document.getElementById('showImg');
+  let showUser = document.getElementById('showUser');
+  let logOutBtn = document.getElementById('logOut');
+  let cont = document.getElementById('hide-container');
+  let btnCont =document.getElementById('container');
+  /*!!!!!!!!!!!!!!knapp events!!!!!!!!!!!!!!!!!*/
+  authBtn.addEventListener('click', function(event) {
+     cont.style.display="block";
+     btnCont.style.visibility="hidden";
+	 logInUser();
+  });
+  logOutBtn.addEventListener('click', function(event) {
+	cont.style.display="none";
+	btnCont.style.visibility="visible";
+	logOutUser();
+  })
+  
+
