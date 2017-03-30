6768065cc0e6c68be90342a7ebb90c8bc8948c97
sebastian <estradis_91@hotmail.com>
2017-03-30 09:48:28 +0200

    nydag

--- Ny mapp/authcheck.js
+++ Ny mapp/authcheck.js
@@ -53,9 +53,10 @@
   });
   secBtn.addEventListener('click', function(event) {
 	  console.log("secret klick")
-	 if( firebase.auth().currentUser.providerData[0].displayName != null ) {
+	 if( firebase.auth().currentUser.providerData[0].displayName !== null ) {
 		 secImg.style.visibility="visible";
 		 secImg.src="img/saywhatagain.jpg";
+		 console.log("bilden visas!")
 	 }
 		 secImg.style.visibility="hidden";
 	 
